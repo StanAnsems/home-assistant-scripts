781fac1f31a351c248f967c2c2d4f1f501a5e3e0
Stan Ansems <stanansems@hotmail.com>
2024-01-10 10:54:15 +0100

    Create temp-mqtt-wifi.ino

---
+++ temp-mqtt-wifi.ino
@@ -0,0 +1,139 @@
+#include <DHT.h>
+#include "ESP8266WiFi.h"
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+
+//Constants
+#define DHTPIN 4     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+const char* WIFI_SSID = ""; // Write here your router's username
+const char* WIFI_PASSWORD = ""; // Write here your router's password
+
+#define MQTT_HOST IPAddress(192, 168, 0, 17)
+#define MQTT_PORT 1883
+#define MQTT_USER ""
+#define MQTT_PASS ""
+#define MQTT_PUB_TEMP "esp/dht/temperature"
+#define MQTT_PUB_HUM "esp/dht/humidity"
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 10000;        // Interval at which to publish sensor readings
+int publishCount = 0;               // Count number of messages to be published
+
+bool hasPublished = false;          // Did we publish messages
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  publishCount--;
+  Serial.printf("Message count after publish ack %i \n", publishCount);
+}
+
+void setup() {
+  Serial.begin(9600);
+	dht.begin();
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  // If your broker requires authentication (username and password), set them below
+  mqttClient.setCredentials(MQTT_USER, MQTT_PASS);
+  connectToWifi();
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  unsigned long currentMillis = millis();
+
+  // Every X number of seconds (interval = 10 seconds)
+  // it publishes a new MQTT message
+  if (!hasPublished && currentMillis - previousMillis >= interval) {
+    if (!WiFi.isConnected()) {
+      Serial.printf("Wifi is not connected \n");
+      return;
+    }
+
+    if (!mqttClient.connected()) {
+      Serial.printf("MQTT not connected \n");
+      return;
+    }
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // New DHT sensor readings
+    hum = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    temp = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //temp = dht.readTemperature(true);
+
+    // Publish an MQTT message on topic esp/dht/temperature
+    publishCount++;
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_TEMP, packetIdPub1);
+    Serial.printf("Message: %.2f \n", temp);
+
+    // Publish an MQTT message on topic esp/dht/humidity
+    publishCount++;
+    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+    Serial.printf("Message: %.2f \n", hum);
+    hasPublished = true;
+    Serial.printf("Message count %i \n", publishCount);
+  }
+  if (hasPublished && publishCount == 0) {
+    Serial.printf("All messages published\n");
+    ESP.deepSleep(30e6);                        // Go into deep sleep for 30 sec to reduce power and heating
+  }
+}
